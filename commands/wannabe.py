59b85d2cd3debdb0d3c35fa7ea6a5cf6dd884467
StaiLee <ilyesstaili@gmail.com>
2024-01-06 16:11:04 +0100

    add myStats function

--- commands/wannabe.py
+++ commands/wannabe.py
@@ -13,6 +13,15 @@ intents.message_content = True
 Aeonya = commands.Bot(command_prefix="*", intents=discord.Intents.all())
 
 
+async def get_class_stats(class_name):
+    async with aiosqlite.connect('aeon.db') as db:
+        cursor = await db.execute('''
+            SELECT * FROM class_info WHERE class_name = ?
+        ''', (class_name,))
+        stats = await cursor.fetchone()
+        return stats  # Retourne les statistiques de la classe choisie
+
+
 
 @Aeonya.command()
 async def wannabeMageimport(ctx):
@@ -119,5 +128,41 @@ async def wannabeArcherimport(ctx):
         async with Aeonya.db.execute("INSERT INTO class (user_id, chosen_class) VALUES (?, 'archer')", (user_id,)) as cursor:
             await Aeonya.db.commit()
             await ctx.send(f"{ctx.author.name}, you have chosen the Archer class. Your adventure as an archer begins!")
+
+@Aeonya.command()
+async def myStatsimport(ctx):
+    user_id = ctx.author.id
+
+    async with aiosqlite.connect('aeon.db') as db:
+        cursor = await db.execute("SELECT chosen_class FROM class WHERE user_id = ?", (user_id,))
+        existing_class = await cursor.fetchone()
+
+        if existing_class:
+            chosen_class = existing_class[0]
+
+            class_stats = await db.execute_fetchall(
+                "SELECT * FROM class_info WHERE class_name = ?", (chosen_class,)
+            )
+
+            if class_stats:
+                class_stats = class_stats[0]  # Il ne devrait y avoir qu'une seule ligne
+
+                embed_stats = discord.Embed(
+                    title=f"{ctx.author.name}'s Stats as {chosen_class.capitalize()}",
+                    color=0x9a939c
+                )
+
+                embed_stats.add_field(name="Class", value=chosen_class.capitalize(), inline=False)
+                embed_stats.add_field(
+                    name="Base Stats",
+                    value=f"HP: {class_stats[1]}\nSTAM: {class_stats[2]}\nDEX: {class_stats[3]}\nSTR: {class_stats[4]}\nINT: {class_stats[5]}",
+                    inline=False
+                )
+
+                await ctx.send(embed=embed_stats)
+            else:
+                await ctx.send("No stats found for your class.")
+        else:
+            await ctx.send("You haven't chosen a class yet.")
     
 
